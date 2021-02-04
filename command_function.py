f4a0889a3f27579d1381bf83de1a088619ca3e3d
chanhohan <chhqm2759@naver.com>
2021-02-04 23:12:56 +0900

    [MOD]command_function

--- command_function.py
+++ command_function.py
@@ -1,6 +1,10 @@
+import jwt
+import uuid
+import hashlib
+from urllib.parse import urlencode
+import requests
 import telegram
 from telegram.ext import Updater, CommandHandler
-
 
 def command_check(self, update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="듣고 있습니다.")
@@ -11,3 +15,36 @@ def command_stop(self, update, context):
     self.update.dispatcher.stop()
     self.update.job_queue.stop()
     self.update.stop()
+
+def command_buy_sell(*args):
+    server_url = args[0]
+    access_key = args[1]
+    secret_key = args[2]
+    user_price = args[3][0]
+    buy_sell = args[3][1]
+    coin = args[3][2]
+
+    query = {
+        'market': 'KRW-' + coin,
+        'side': buy_sell,
+        'price': user_price,
+        'ord_type': 'price',
+    }
+
+    query_string = urlencode(query).encode()
+    m = hashlib.sha512()
+    m.update(query_string)
+    query_hash = m.hexdigest()
+
+    payload = {
+        'access_key': access_key,
+        'nonce': str(uuid.uuid4()),
+        'query_hash': query_hash,
+        'query_hash_alg': 'SHA512',
+    }
+    jwt_token = jwt.encode(payload, secret_key)
+    authorize_token = 'Bearer {}'.format(jwt_token)
+    headers = {"Authorization": authorize_token}
+    res = requests.post(server_url + "/v1/orders", params=query, headers=headers)
+
+    return res
