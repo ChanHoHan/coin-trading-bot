444f1c5eda96441c59e2fbc382558dfb262df006
leegun <akstp88@gmail.com>
2021-02-14 21:58:57 +0900

    [MOD]multiprocess_func

--- bot_.py
+++ bot_.py
@@ -1,34 +1,48 @@
 import requests
-import time
+from time import sleep
 from multiprocessing import Process, Queue
 
-coin_name = "KRW-"
 desired_percent = 0
 
 
 def ticker(coin_name):
-    coin_name = "KRW-"+coin_name
     url = "https://api.upbit.com/v1/ticker"
-    res = requests.request("GET", url, params={"markets":coin_name})
+    res = requests.request("GET", url, params={"markets": "{}".format(coin_name)})
     return res.json()
 
 
-def loop_limit(desired_price, coin):
-    response = ticker(coin)
-    present_price = float(response[0]['trade_price'])
-    if desired_price >= present_price:
-        print("하한선에 도착하였습니다.")
-        exit()
-    print("현재 가격: "+str(present_price))
-    time.sleep(10)
+def my_multiprocess():
+    coin_name = "KRW-"
+    flag = False
+    while not flag:
+        file_input = open("./input.txt", 'r')
+        input_lines = file_input.readlines()
+        if len(input_lines) == 2:
+            coin_name = coin_name + input_lines[0]
+            limit = float(input_lines[1])
+            res = ticker(coin_name)
+            start_price = float(res[0]['trade_price'])
+            while True:
+                res = ticker(coin_name)
+                present_price = res[0]["trade_price"]
+                file_output = open("./output.txt", 'w')
+                file_output.write(str(present_price))
+                print(present_price)
+                file_output.close()
+                desired_price = (start_price / 100) * (100 - limit)
+                if desired_price >= present_price:
+                    print("하한선에 도착하였습니다.")
+                    file_output = open("./output.txt", 'w')
+                    file_output.write("end")
+                    file_output.close()
+                    flag = True
+                    break
+                sleep(10)
+        file_input.close()
+        sleep(10)
 
 
 if __name__ == "__main__":
-    coin_name += input("코인 이름을 약어로 입력하시오. ex)BTC\n")
-    desired_percent = float(input("하한선을 퍼센트로 입력하시오.\n"))
-    response = ticker(coin_name)
-    start_price = float(response[0]['trade_price'])
-    desired_price = (start_price / 100) * (100-desired_percent)
-    pr1 = Process(target=loop_limit, args=(desired_price, coin_name))
+    pr1 = Process(target=my_multiprocess)
     pr1.start()
     pr1.join()
